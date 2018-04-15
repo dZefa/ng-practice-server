efa7a01abe1c8af6c3c1d8abbab7ae288d5b1877
Daniel Chong <danchong625@gmail.com>
2018-04-15 14:38:15 -0700

    [ops] implements db sync

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import syncDB from '../database/util/sync';
 import router from './router';
 
 const app = express();
@@ -18,5 +19,7 @@ app.use(express.static(path.resolve(__dirname, process.env.CLIENT_PATH)));
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, process.env.CLIENT_INDEX_PATH));
 });
+
+syncDB();
 
 export default app;
